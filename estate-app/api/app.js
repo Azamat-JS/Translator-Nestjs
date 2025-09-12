98a9646c7a80185d4b975f94a85075c98aa42359
Azamat-JS <azamatielts987@gmail.com>
2025-09-12 16:49:26 +0500

    implemented routes and installed prisma

--- estate-app/api/app.js
+++ estate-app/api/app.js
@@ -1,10 +1,12 @@
 import express from 'express'
-
+import authRouter from './routes/auth.route.js';
+import postRouter from './routes/post.route.js';
 const app = express();
 
-app.use('/api/test', (req, res) => {
-    res.json({message: "salom"})
-})
+
+app.use(express.json())
+app.use('/api/auth', authRouter);
+app.use('/api/posts', postRouter);
 app.listen(8800, () => {
     console.log('Server is running')
 })
---
+++ estate-app/api/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+import bcrypt from 'bcryptjs'
+
+
+export const  register = async(req, res) => {
+   const {username, email, password} = req.body;
+   
+   const hashedPassword = await bcrypt.hash(password, 10);
+   console.log(hashedPassword)
+}
+export const login = (req, res) => {
+
+}
+export const logout = (req, res) => {
+
+}
