39453ea2d461902842746a320a70a011b41f5eb2
Azamat-JS <azamatielts987@gmail.com>
2025-09-12 15:50:22 +0500

    Started real-estate app

---
+++ estate-app/api/app.js
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const app = express();
+
+app.use('/api/test', (req, res) => {
+    res.json({message: "salom"})
+})
+app.listen(8800, () => {
+    console.log('Server is running')
+})
