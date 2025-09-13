79b8f396ed17dec369f1594fa83adbda26ed5e2e
Azamat-JS <azamatielts987@gmail.com>
2025-09-13 12:17:08 +0500

    Implemented mongodb in estate app

--- estate-app/api/controllers/auth.controller.js
+++ estate-app/api/controllers/auth.controller.js
@@ -1,11 +1,19 @@
 import bcrypt from 'bcryptjs'
+import prisma from '../lib/prisma';
 
 
 export const  register = async(req, res) => {
    const {username, email, password} = req.body;
    
    const hashedPassword = await bcrypt.hash(password, 10);
-   console.log(hashedPassword)
+   console.log(hashedPassword);
+   const newUser = await prisma.user.create({
+      data: {
+         username,
+         email,
+         password
+      }
+   })
 }
 export const login = (req, res) => {
 
