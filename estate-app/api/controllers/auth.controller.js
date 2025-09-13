f1252c3e3b4a9850fd507a1f0911593c8ae1f3e7
Azamat-JS <azamatielts987@gmail.com>
2025-09-13 14:36:20 +0500

    implemented cookie-parser in app.js

--- estate-app/api/controllers/auth.controller.js
+++ estate-app/api/controllers/auth.controller.js
@@ -1,23 +1,48 @@
 import bcrypt from 'bcryptjs'
 import prisma from '../lib/prisma.js';
+import jwt from 'jsonwebtoken'
 
 
 export const  register = async(req, res) => {
    const {username, email, password} = req.body;
-   
-   const hashedPassword = await bcrypt.hash(password, 10);
-   console.log(hashedPassword);
-   const newUser = await prisma.user.create({
-      data: {
-         username,
-         email,
-         password: hashedPassword,
-      }
-   });
-   res.status(200).json(newUser)
+   try {   
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const newUser = await prisma.user.create({
+         data: {
+            username,
+            email,
+            password: hashedPassword,
+         }
+      });
+      res.status(201).json({message: "User created successfully"});
+   } catch (error) {
+      res.status(500).json({message: 'Failed to create user!'})
+   }
 }
-export const login = (req, res) => {
-
+export const login = async(req, res) => {
+const {username, password} = req.body;
+try {
+   const user = await prisma.user.findUnique({
+      where: {username}
+   }, );
+   
+   if(!user) return res.status(401).json({message: 'Invalid credentials!'});
+   const isPasswordValid = await bcrypt.compare(password, user.password);
+   
+   if(!isPasswordValid) return res.status(401).json({message: 'Invalid credentials!'});
+   const token = jwt.sign({
+      id: user.id,
+   }, process.env.JWT_SECRET_KEY)
+const age = 1000 * 3600 * 24 * 7;
+res.cookie('test2', 'myvalue2', {
+   httpOnly: true,
+   // secure: true,
+   maxAge: age,
+}).status(200).json({message: 'Login successful'})
+} catch (error) {
+   console.log(error);
+   res.status(500).json({message: 'Failed to login!'})
+}
 }
 export const logout = (req, res) => {
 
