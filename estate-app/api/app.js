cf6252a6c3a35c681cd40923385dbe0c0cc80f2d
Azamat-JS <azamatielts987@gmail.com>
2025-09-16 15:22:09 +0500

    Added client side in estate app

--- estate-app/api/app.js
+++ estate-app/api/app.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import authRouter from './routes/auth.route.js';
 import postRouter from './routes/post.route.js';
 const app = express();
 
-
+app.use(cors({origin:"*"}))
 app.use(express.json());
 app.use(cookieParser())
 app.use('/api/auth', authRouter);
--- estate-app/api/controllers/auth.controller.js
+++ estate-app/api/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import bcrypt from 'bcryptjs'
 import prisma from '../lib/prisma.js';
 import jwt from 'jsonwebtoken'
 
+const age = 1000 * 3600 * 24 * 7;
 
 export const  register = async(req, res) => {
    const {username, email, password} = req.body;
@@ -14,6 +15,7 @@ export const  register = async(req, res) => {
             password: hashedPassword,
          }
       });
+      console.log(newUser)
       res.status(201).json({message: "User created successfully"});
    } catch (error) {
       res.status(500).json({message: 'Failed to create user!'})
@@ -32,9 +34,8 @@ try {
    if(!isPasswordValid) return res.status(401).json({message: 'Invalid credentials!'});
    const token = jwt.sign({
       id: user.id,
-   }, process.env.JWT_SECRET_KEY)
-const age = 1000 * 3600 * 24 * 7;
-res.cookie('test2', 'myvalue2', {
+   }, process.env.JWT_SECRET_KEY, {expiresIn: age})
+res.cookie('token', token, {
    httpOnly: true,
    // secure: true,
    maxAge: age,
@@ -45,5 +46,5 @@ res.cookie('test2', 'myvalue2', {
 }
 }
 export const logout = (req, res) => {
-
+res.clearCookie('token').status(200).json({message: 'Logout', })
 }
